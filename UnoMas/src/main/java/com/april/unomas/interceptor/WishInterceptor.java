fe0d77c51cb1d55ec20466be578c30249f73ae57
Nomad4512 <zizonbini@gmail.com>
2022-05-18 13:20:59 +0900

    [hyunbin] wishlist 로그인 인터셉터 구현완료

--- UnoMas/src/main/java/com/april/unomas/interceptor/WishInterceptor.java
+++ UnoMas/src/main/java/com/april/unomas/interceptor/WishInterceptor.java
@@ -1,32 +1,47 @@
 package com.april.unomas.interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class WishInterceptor implements HandlerInterceptor{
 
+	// wishlist 작업 전 로그인이 안되어있으면 로그인화면으로 보내기
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+			throws Exception { 
+		// 세션 가져오기
+		HttpSession session = request.getSession();
+		
+		// 로그인이 안되어있다면
+		if(session.getAttribute("saveID") == null) {
+			
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter printwriter = response.getWriter();
+			printwriter.println("<script>alert('로그인이 필요합니다.'); location.href='/user/login';</script>");
+			printwriter.flush();
+			printwriter.close(); 
+			
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
+
 
 }
