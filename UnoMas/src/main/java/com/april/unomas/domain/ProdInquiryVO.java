77c8dd40663775810b3a76a6d4882a9737a8f1ae
Yujin Hwang <miro7923@gmail.com>
2022-05-27 12:33:08 +0900

    Update ProdInquiryVO.java

--- UnoMas/src/main/java/com/april/unomas/domain/ProdInquiryVO.java
+++ UnoMas/src/main/java/com/april/unomas/domain/ProdInquiryVO.java
@@ -11,7 +11,10 @@ public class ProdInquiryVO {
 	private Integer user_num;
 	private String user_id;
 	private Integer prod_num;
+	private String prod_name;
 	private String p_inquiry_title;
 	private String p_inquiry_content;
 	private Timestamp p_inquiry_regdate;
+	private boolean p_inquiry_processs;
+	private ProdCommentVO prod_comment;
 }
