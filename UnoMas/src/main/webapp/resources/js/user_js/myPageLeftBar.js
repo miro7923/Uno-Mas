ffe3dc4a26a9b7833ed9490d1b026f8de2010fe5
luanaeun <tksop59@naver.com>
2022-05-31 00:29:46 +0900

    [seongeun]마이페이지 사이드바 완성

--- UnoMas/src/main/webapp/resources/js/user_js/myPageLeftBar.js
+++ UnoMas/src/main/webapp/resources/js/user_js/myPageLeftBar.js
@@ -1,11 +1,5 @@
 $(function(){
 	let total_url = new URL(window.location.href);
 	let current_url = total_url.pathname;
-
-//	$("a[href$="+ current_url + "]").css("color","red");
-//	console.log("이게 나오나.." + $("a[href$="+ current_url + "]"))
-	if($("a").attr("href") == current_url) {
-		$("a[href="+ current_url + "]").css('color','blue');
-		
-	}
+	$(`a[href='${current_url+total_url.search}']`).css({'color':'#B9CE45', 'font-weight':'bold'});
  });
