3ba8f7cb4605c0027abac4c399d444dd32e844fa
OHEUNHYEON <dmsgus555@gmail.com>
2022-05-31 23:04:56 +0900

    ..

--- UnoMas/src/main/webapp/resources/js/admin_js/adminSearch.js
+++ UnoMas/src/main/webapp/resources/js/admin_js/adminSearch.js
@@ -5,7 +5,7 @@
  	var search_type_val = document.getElementById("search_type");
    	var type_val = search_type_val.options[search_type_val.selectedIndex].value;
    	var keyword_val = document.getElementById("keyword").value;
-   	var url = "/admin/faq_board?search_type="+type_val+"&keyword="+encodeURIComponent(keyword_val);
+   	var url = "/UnoMas/admin/faq_board?search_type="+type_val+"&keyword="+encodeURIComponent(keyword_val);
     		
     location.href=url;
 }
@@ -14,7 +14,7 @@ function search_admin_notice() {
     		var search_type_val = document.getElementById("search_type");
     		var type_val = search_type_val.options[search_type_val.selectedIndex].value;
     		var keyword_val = document.getElementById("keyword").value;
-    		var url = "/admin/notice_board?search_type="+type_val+"&keyword="+encodeURIComponent(keyword_val);
+    		var url = "/UnoMas/admin/notice_board?search_type="+type_val+"&keyword="+encodeURIComponent(keyword_val);
     		
     		location.href=url;
     	}
